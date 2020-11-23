c45e012877ec5156c188bfd54d33f0f8d93c982b
Hodosov <Hodosov.y@mail.ru>
2020-11-23 22:10:11 +0300

    add methods in dom.js and fix Exel

--- src/components/excel/Excel.js
+++ src/components/excel/Excel.js
@@ -2,17 +2,16 @@ import { $ } from '../../core/dom'
 
 export class Excel {
     constructor(selector, options) {
-        this.$el = document.querySelector(selector)
+        this.$el = $(selector)
         this.components = options.components || []
     }
 
     getRoot() {
         const $root = $.create('div', 'excel')
-
         this.components.forEach(Component => {
             const $el = $.create('div', Component.className)
             const component = new Component($el)
-            $el.innerHTML = component.toHTML()
+            $el.html(component.toHTML())
             $root.append($el)
         })
 
@@ -23,3 +22,4 @@ export class Excel {
         this.$el.append(this.getRoot())
     }
 }
+ 
