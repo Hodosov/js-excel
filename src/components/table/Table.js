20c902ae7d4cb994bea44c49bb7d439243582a25
Hodosov <Hodosov.y@mail.ru>
2020-12-05 21:54:38 +0300

    add selection of cells

--- src/components/table/Table.js
+++ src/components/table/Table.js
@@ -1,7 +1,10 @@
 import { ExcelComponent } from '../../core/ExcelComponent';
 import { createTable } from './table.tamplate';
 import { resizeHandler } from './table.resize';
-import { shoutResize } from './table.functions'
+import { isCell, shoutResize } from './table.functions'
+import { TableSelection } from './TableSelection';
+import { matrix } from './table.functions'
+import { $ } from '../../core/dom'
 
 export class Table extends ExcelComponent {
     static className = 'excel__table'
@@ -16,9 +19,28 @@ export class Table extends ExcelComponent {
         return createTable()
     }
 
+    prepare() {
+        this.selection = new TableSelection()
+    }
+
+    init() {
+        super.init()
+        const $cell = this.$root.find('[data-id="0:0"]')
+        this.selection.select($cell)
+    }
+
     onMousedown(event) {
         if (shoutResize(event)) {
             resizeHandler(this.$root, event)
+        } else if (isCell(event)) {
+            const $target = $(event.target)
+            if (event.shiftKey) {
+                const $cells = matrix($target, this.selection.current)
+                    .map(id => this.$root.find(`[data-id="${id}"]`))
+                this.selection.selectGroup($cells)                
+            } else {
+                this.selection.select($target)
+            }
         }
     }
 }
--- src/components/table/table.tamplate.js
+++ src/components/table/table.tamplate.js
@@ -3,10 +3,18 @@ const CODES = {
     Z: 90
 }
 
-function toCell(_, col) {
-    return `
-        <div class='cell' contenteditable data-col="${col}"></div>
+function toCell(row) {
+    return function(_, col) {
+        return `
+            <div 
+            class='cell' 
+            contenteditable
+            data-type="cell"
+            data-col="${col}" 
+            data-id="${row}:${col}"
+            ></div>
     `
+    }
 }
 
 function toColumn(col, index) {
@@ -19,9 +27,9 @@ function toColumn(col, index) {
 }
 
 function createRow(index, content) {
-    const resize = index 
-    ? "<div class='row-resize' data-resize='row'></div>" 
-    : ''
+    const resize = index
+        ? "<div class='row-resize' data-resize='row'></div>"
+        : ''
 
     return `
         <div class='row' data-type='resizable'>
@@ -50,13 +58,13 @@ export function createTable(rowsCount = 25) {
 
     rows.push(createRow(null, cols))
 
-    for (let i = 0; i < rowsCount; i++) {
+    for (let row = 0; row < rowsCount; row++) {
         const cells = new Array(colsCount)
-        .fill('')
-        .map(toCell)
-        .join('')
+            .fill('')
+            .map(toCell(row))
+            .join('')
 
-        rows.push(createRow(i + 1, cells))
+        rows.push(createRow(row + 1, cells))
     }
 
     return rows.join('')
